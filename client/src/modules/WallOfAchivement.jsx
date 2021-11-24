1bd958d9f5628a0ba6409f486587e68d38a8583a
1157030004 <1157030004@student.uinsgd.ac.id>
2021-11-24 14:32:50 +0700

    fixed fetch achievements issue

--- client/src/modules/WallOfAchivement.jsx
+++ client/src/modules/WallOfAchivement.jsx
@@ -4,19 +4,23 @@ import Card from "../components/Card";
 const WallOfAchivement = ({ source, tab }) => {
 	return (
 		<div className="w-auto flex flex-wrap ">
-			{source.map((item) => (
-				<Card
-					key={item._id}
-					id={item._id}
-					title={item.title}
-					activity={item.activity}
-					level={item.level}
-					score={item.score}
-					status={item.status}
-					proof={item.proof}
-					tab={tab}
-				/>
-			))}
+			{source.length >= 0 ? (
+				source.map((item) => (
+					<Card
+						key={item._id}
+						id={item._id}
+						title={item.title}
+						activity={item.activity}
+						level={item.level}
+						score={item.score}
+						status={item.status}
+						proof={item.proof}
+						tab={tab}
+					/>
+				))
+			) : (
+				<div>fethcing...</div>
+			)}
 		</div>
 	);
 };
